157d6e7ddef603d7d7a96b91594ae9cbaa38ae07
osamanazar <osamanazar47@gmail.com>
2024-01-20 06:56:51 -0800

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *a, it *b)
+{
+	int t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
+/**
+ * bubble_sort - a function that sorts an array of integers
+ * @array: the array to be sorted
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j = size - i - 1; j++)
+			swap(&array[j], &array[j + 1]);
+	}
+	print_array(array, size);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+                            Sorting algorithms & Big O
+
+This is a project about sorting algorithms and implementing some of them
+
+Each file contains an implementation of an algorithm
+
+0-bubble_sort.c: implementing the bubble sorting
