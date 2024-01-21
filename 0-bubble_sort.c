26ebb6b61b09fef0ce9b52358f39aa73c026fe99
osamanazar <osamanazar47@gmail.com>
2024-01-21 07:03:45 -0800

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,7 +24,10 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
-			swap(&array[j], &array[j + 1]);
+		{
+			if (array[j] > array[j + 1])
+				swap(&array[j], &array[j + 1]);
+		}
 		print_array(array, size)
 	}
 }
