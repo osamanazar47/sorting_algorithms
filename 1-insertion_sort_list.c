572b391b4db373724ee43994dc725d8be8fc8098
osamanazar <osamanazar47@gmail.com>
2024-01-21 09:04:57 -0800

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - a function that sorts 
+ * a doubly linked list of integers in 
+ * ascending order using the Insertion sort algorithm
+ * @list: the list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp, *prev, *next;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current;
+
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			prev = temp->prev;
+			next = temp->next;
+
+			if (prev->prev !=NULL)
+				prev->prev->next = temp;
+			temp->prev = prev->prev;
+			temp->next = prev;
+			prev->prev = temp;
+			prev->next = next;
+			if (next != NULL)
+				next->prev = prev;
+			if (temp->prev == NULL)
+				*list = temp;
+		}
+		current = current->next;
+		print_list(*list);
+	}
+}
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is a project about sorting algorithms and implementing some of them
 Each file contains an implementation of an algorithm
 
 0-bubble_sort.c: implementing the bubble sorting
+
+1-insertion_sort_list.c:  a function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm
