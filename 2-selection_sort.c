dc6d8cadd7dce1a2c7e8242af63c7d8e883f50e7
osamanazar <osamanazar47@gmail.com>
2024-01-22 05:26:09 -0800

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,19 +19,23 @@ void swap_e(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
+	size_t i, j;
+	int *min_index;
 
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = i;
+		min_index = array + i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_index])
-				min_index = j;
+			if (array[j] < *min_index)
+				min_index = array + j;
 		}
-		swap_e(&array[i], &array[min_index]);
-		print_array(array, size);
+		if (min_index != array + i)
+		{
+			swap_e(array + i, min_index);
+			print_array(array, size);
+		}
 	}
 }
